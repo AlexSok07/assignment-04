7f7bad0ae61862a6ce64504f55e18ddad8b3fe2c
HDisHERE <125623145+HDisHERE@users.noreply.github.com>
2026-04-10 11:14:12 -0400

    Some final issues are fixed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
-import Home from './components/Home.vue'
+
+const route = useRoute()
+const isHomePage = computed(() => route.path === '/')
 </script>
 
 <template>
-  <div class="page">
+  <div class="page" :class="{ 'texture-homepage': isHomePage }">
     <Navbar />
 
     <RouterView />
